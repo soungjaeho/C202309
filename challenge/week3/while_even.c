dfe5f5a17254caf398246db159357a05d30bbbdd
soungjaeho <144207602+soungjaeho@users.noreply.github.com>
2023-09-18 16:43:36 +0900

    Update while_even.c

--- challenge/week3/while_even.c
+++ challenge/week3/while_even.c
@@ -2,13 +2,19 @@
 
 int main() {
 	int i = 0;
+	// 변수 i 선언
 	while (i < 10) {
+		// i 가 10미만까지 반복
 		i++;
+		// i 를 1씩 증가
 		if (i % 2 == 0) {
 			continue; 
+// i 가 짝수면 처음으로
 		}
 		printf("%d Hello World!\n", i++);
+	// 홀수면 출력
 	}
+	
 
 	return 0;
 
