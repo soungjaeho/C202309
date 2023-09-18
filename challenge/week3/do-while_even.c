9b39e5cd35e6b98310415b5df25d1bf26ab61718
soungjaeho <144207602+soungjaeho@users.noreply.github.com>
2023-09-18 16:42:31 +0900

    Update do-while_even.c

--- challenge/week3/do-while_even.c
+++ challenge/week3/do-while_even.c
@@ -2,15 +2,18 @@
 
 int main() {
 	int i = 0;
-
+//변수 i 선언
 	do {
 		i++;
+		// i 를 1씩 증가
 		if (i % 2 == 0) {
 			continue;
+			// i가 짝수면 처음으로
 		}
 		printf("%d Hello World!\n", i);
-
+// i 가 홀수면 출력
 	} while (i < 10); 
+	// i 가 10미만까지 반복
 
 
 	return 0;
